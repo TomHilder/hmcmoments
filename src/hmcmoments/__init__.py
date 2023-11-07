6826175633365c323db06ef78d9bccfff43aa433
Tom Hilder <tom.hilder.dlhp@gmail.com>
2023-11-07 13:33:06 +1100

    initial package structure

---
+++ src/hmcmoments/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+# Written by Thomas Hilder
+
+"""
+TODO: Add Package description
+"""
+
+# Package version
+__version__ = "0.0.1"
+
+# User accessible classses and functions go here
+...
---
+++ src/hmcmoments/__main__.py
@@ -0,0 +1,14 @@
+# __main__.py
+# Writen by Thomas Hilder
+
+"""
+Command line interface for hmcmoments.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
