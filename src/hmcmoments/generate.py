7d8840452407362d1bb2466d96ac7f295a6c5e18
Tom Hilder <tom.hilder.dlhp@gmail.com>
2023-11-08 14:04:50 +1100

    functions for trimming and downsampling written and working

--- src/hmcmoments/generate.py
+++ src/hmcmoments/generate.py
@@ -9,10 +9,9 @@ from copy import copy
 
 import matplotlib.pyplot as plt
 import numpy as np
-from astropy.io import fits
 from skimage.measure import block_reduce
 
-from .image import estimate_rms
+from .image import downsample_cube, estimate_rms, trim_cube
 from .io import previous_results_exist, read_cube
 from .settings import Settings
 
@@ -33,62 +32,28 @@ def generate(settings: Settings) -> None:
 
     # TRIM CUBE
 
-    peak_intensity = np.max(np.nan_to_num(image), axis=0)
-
-    summed_y = np.max(peak_intensity, axis=1)
-    summed_x = np.max(peak_intensity, axis=0)
-
-    threshold_x = 1.5 * np.median(summed_y)
-    threshold_y = 1.5 * np.median(summed_x)
-
-    i_x_min = np.argmin(summed_x < threshold_x) - 10
-    i_y_min = copy(i_x_min)
-
-    i_x_max = image.shape[1] - i_x_min
-    i_y_max = copy(i_x_max)
-
-    trimmed_image = image[:, i_y_min:i_y_max, i_x_min:i_x_max]
-
-    print(i_y_min, i_y_max)
-    print(i_x_min, i_x_max)
-
-    trimmed_x_axis = x_axis[i_x_min:i_x_max]
-    trimmed_y_axis = y_axis[i_y_min:i_y_max]
+    trimmed_x_axis, trimmed_y_axis, trimmed_image = trim_cube(
+        x_axis=x_axis, y_axis=y_axis, image=image
+    )
 
     # DOWNSAMPLE
 
-    BLOCK_SIZE = settings.downsample
-
-    if BLOCK_SIZE > 1:
-        ds_trimmed_shape = block_reduce(
-            trimmed_image[0, :, :], block_size=BLOCK_SIZE, func=np.mean
-        ).shape
-
-        ds_trimmed_image = np.zeros((image.shape[0], *ds_trimmed_shape))
-
-        for i in range(ds_trimmed_image.shape[0]):
-            ds_trimmed_image[i, :, :] = block_reduce(
-                trimmed_image[i, :, :], block_size=BLOCK_SIZE, func=np.mean
-            )
-
-        ds_trimmed_x_axis = block_reduce(
-            trimmed_x_axis, block_size=BLOCK_SIZE, func=np.mean
-        )
-        ds_trimmed_y_axis = block_reduce(
-            trimmed_y_axis, block_size=BLOCK_SIZE, func=np.mean
-        )
-
-        ds_trimmed_x_axis[-1] = ds_trimmed_x_axis[-2] + (
-            ds_trimmed_x_axis[-2] - ds_trimmed_x_axis[-3]
-        )
-        ds_trimmed_y_axis[-1] = ds_trimmed_y_axis[-2] + (
-            ds_trimmed_y_axis[-2] - ds_trimmed_y_axis[-3]
+    if settings.downsample > 1:
+        ds_trimmed_x_axis, ds_trimmed_y_axis, ds_trimmed_image = downsample_cube(
+            x_axis=trimmed_x_axis,
+            y_axis=trimmed_y_axis,
+            image=trimmed_image,
+            block_size=settings.downsample,
         )
 
     else:
         ds_trimmed_x_axis = trimmed_x_axis
         ds_trimmed_y_axis = trimmed_y_axis
         ds_trimmed_image = trimmed_image
+
+    plt.plot(ds_trimmed_x_axis)
+    plt.plot(ds_trimmed_y_axis)
+    plt.show()
 
     plt.pcolormesh(
         ds_trimmed_x_axis,
--- src/hmcmoments/image.py
+++ src/hmcmoments/image.py
@@ -7,12 +7,17 @@ Functions to manipulate cube image data.
 
 import numpy as np
 from numpy.typing import NDArray
+from skimage.measure import block_reduce
 
 from .settings import Settings
+
+# Function for downsampling
+DS_FUNC = np.mean
 
 
 # Estimate the noise in a cube
 def estimate_rms(image: NDArray, settings: Settings) -> float:
+    # Estimate RMS using standard deviation of channels far from systemic velocity
     return float(
         np.std(
             a=np.concatenate(
@@ -27,13 +32,64 @@ def estimate_rms(image: NDArray, settings: Settings) -> float:
 
 # Trim cube image down to region containing disc
 def trim_cube(
-    x_axis: NDArray, y_axis: NDArray, image: NDArray
+    x_axis: NDArray,
+    y_axis: NDArray,
+    image: NDArray,
 ) -> tuple[NDArray, NDArray, NDArray]:
-    pass
+    # Replace all NaNs in image with zeros
+    image = np.nan_to_num(image, nan=0.0)
+    # Calculate peak intensity along each spatial dimension
+    peak_intensity_x = np.max(
+        image,
+        axis=(0, 2),
+    )
+    peak_intensity_y = np.max(
+        image,
+        axis=(0, 1),
+    )
+    # Calculate threshold for inclusion using median from each
+    threshold = 1.5 * np.median(np.concatenate([peak_intensity_x, peak_intensity_y]))
+    # Get indicies to make cuts (enforce square image by using same in each dimension)
+    i_min = np.argmin(peak_intensity_x < threshold) - 50
+    i_max = image.shape[1] - i_min
+    # Return trimmed results
+    return (
+        x_axis[i_min:i_max],
+        y_axis[i_min:i_max],
+        image[:, i_min:i_max, i_min:i_max],
+    )
 
 
-# Downsample cube image
+# Downsample cube image in spatial dimensions
 def downsample_cube(
-    x_axis: NDArray, y_axis: NDArray, image: NDArray
+    x_axis: NDArray,
+    y_axis: NDArray,
+    image: NDArray,
+    block_size: int,
 ) -> tuple[NDArray, NDArray, NDArray]:
-    pass
+    # Constant arguments for downsampling
+    ds_args = dict(block_size=block_size, func=DS_FUNC)
+    # Downsample at one velocity to get dimensions of downsampled image
+    ds_shape = block_reduce(image[0, :, :], **ds_args).shape
+    # Create empty new image of correct shape to fill with downsampled data
+    ds_image = np.zeros((image.shape[0], *ds_shape))
+    # Loop over all velocities and downsample spatially
+    for i in range(ds_image.shape[0]):
+        ds_image[i, :, :] = block_reduce(image[i, :, :], **ds_args)
+    # Downsample coordinates in both spatial axes
+    ds_x_axis = block_reduce(x_axis, **ds_args)
+    ds_y_axis = block_reduce(y_axis, **ds_args)
+
+    # Correct last coordinate for the case where n_pix % block_size != 0 along each dim
+    def correct_axis_after_ds(axis: NDArray) -> NDArray:
+        axis[-1] = axis[-2] + (axis[-2] - axis[-3])
+        return axis
+
+    ds_x_axis = correct_axis_after_ds(ds_x_axis)
+    ds_y_axis = correct_axis_after_ds(ds_y_axis)
+    # Return downsampled results
+    return (
+        ds_x_axis,
+        ds_y_axis,
+        ds_image,
+    )
